0d73b365f4b2224e3eb195017b272fe573e9d722
elsapetillon <elsa.petillon@ipsa.fr>
2023-04-20 17:18:33 +0200

    Ajout d'un bouton

--- interface.py
+++ interface.py
@@ -8,7 +8,14 @@ Authors
 import tkinter as tk 
 
 fenetre = tk.Tk()
+fenetre.title("interface")
+fenetre.geometry("400x300")
+fenetre.configure(bg="#49A")
 
+label = tk.Label(fenetre, text="Variateur angle : ", font=("Helvetica 15 bold"))
+label.pack()
+
+btn = tk.Button(fenetre, text="move", font=("Helvetica 12 bold"), cursor="pirate").pack()
 
 fenetre.mainloop()
 
