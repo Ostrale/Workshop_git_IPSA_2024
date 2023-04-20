2ba445467245678631730f1bf30951580bb73590
Nayel BLIDI <nayel.blidi@ipsa.fr>
2023-04-20 17:36:11 +0200

    #2

--- Workshop_git.py
+++ Workshop_git.py
@@ -6,4 +6,32 @@
 
 import numpy as np
 import xarray as xr
+from datetime import datetime
+
+
+time = np.array(datetime.now())
+time = 0
+x = 0
+y = 0
+angle = 0
+
+array = np.array([time, angle, x, y])
+position = [x, y, time]
+
+lon = x
+lat = y
+
+ds = xr.Dataset(
+    data_vars=dict(
+        position=(["x", "y", "time"], position),
+    ),
+    coords=dict(
+        lon=(["x"], lon),
+        lat=(["y"], lat),
+        time=time,
+    ),
+    attrs=dict(description="Weather related data."),
+    )
+
+
 
